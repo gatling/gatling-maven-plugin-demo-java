eabbdb264058c2a63175adef6c8f1c153f57b1ab
Stephane Landelle <slandelle@gatling.io>
2022-06-22 11:36:33 +0200

    Switch samples from http to https

--- src/test/java/computerdatabase/ComputerDatabaseSimulation.java
+++ src/test/java/computerdatabase/ComputerDatabaseSimulation.java
@@ -82,7 +82,7 @@ public class ComputerDatabaseSimulation extends Simulation {
             .exitHereIfFailed();
 
     HttpProtocolBuilder httpProtocol =
-        http.baseUrl("http://computer-database.gatling.io")
+        http.baseUrl("https://computer-database.gatling.io")
             .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
             .acceptLanguageHeader("en-US,en;q=0.5")
             .acceptEncodingHeader("gzip, deflate")
