39c49d98261fbb0d843d7a66700140dd514564d4
Osanda Deshan Nimalarathna <osanda.deshan@gmail.com>
2023-02-13 17:01:56 +0800

    Update Recorder.java

--- src/test/java/Recorder.java
+++ src/test/java/Recorder.java
@@ -3,13 +3,13 @@ import io.gatling.recorder.config.RecorderPropertiesBuilder;
 import scala.Option;
 
 public class Recorder {
-    public static void main(String[] args) {
-        RecorderPropertiesBuilder props = new RecorderPropertiesBuilder()
-                .simulationsFolder(IDEPathHelper.mavenSourcesDirectory.toString())
-                .resourcesFolder(IDEPathHelper.mavenResourcesDirectory.toString())
-                .simulationPackage("computerdatabase")
-                .simulationFormatJava();
+  public static void main(String[] args) {
+    RecorderPropertiesBuilder props = new RecorderPropertiesBuilder()
+      .simulationsFolder(IDEPathHelper.mavenSourcesDirectory.toString())
+      .resourcesFolder(IDEPathHelper.mavenResourcesDirectory.toString())
+      .simulationPackage("computerdatabase")
+      .simulationFormatJava();
 
-        GatlingRecorder.fromMap(props.build(), Option.apply(IDEPathHelper.recorderConfigFile));
-    }
+    GatlingRecorder.fromMap(props.build(), Option.apply(IDEPathHelper.recorderConfigFile));
+  }
 }
