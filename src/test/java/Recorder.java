535c0eba5542b5db8096921d3cbca3b3277b6df6
Osanda Deshan Nimalarathna <osanda.deshan@gmail.com>
2023-02-13 16:14:43 +0800

    Update Recorder.java
    * Explicit type arguments can be inferred 
    * Cleanup code

--- src/test/java/Recorder.java
+++ src/test/java/Recorder.java
@@ -2,16 +2,14 @@ import io.gatling.recorder.GatlingRecorder;
 import io.gatling.recorder.config.RecorderPropertiesBuilder;
 import scala.Option;
 
-import java.nio.file.Path;
-
 public class Recorder {
-  public static void main(String[] args) {
-    RecorderPropertiesBuilder props = new RecorderPropertiesBuilder()
-      .simulationsFolder(IDEPathHelper.mavenSourcesDirectory.toString())
-      .resourcesFolder(IDEPathHelper.mavenResourcesDirectory.toString())
-      .simulationPackage("computerdatabase")
-      .simulationFormatJava();
+    public static void main(String[] args) {
+        RecorderPropertiesBuilder props = new RecorderPropertiesBuilder()
+                .simulationsFolder(IDEPathHelper.mavenSourcesDirectory.toString())
+                .resourcesFolder(IDEPathHelper.mavenResourcesDirectory.toString())
+                .simulationPackage("computerdatabase")
+                .simulationFormatJava();
 
-    GatlingRecorder.fromMap(props.build(), Option.<Path> apply(IDEPathHelper.recorderConfigFile));
-  }
+        GatlingRecorder.fromMap(props.build(), Option.apply(IDEPathHelper.recorderConfigFile));
+    }
 }
