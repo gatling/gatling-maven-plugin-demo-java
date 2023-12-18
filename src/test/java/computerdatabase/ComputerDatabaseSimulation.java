bc810f5b1450e7d7055f1050a5deb494c5b6a4f5
slandelle <slandelle@gatling.io>
2023-12-18 13:23:18 +0100

    chore: upgrade Gatling 3.10.0

--- src/test/java/computerdatabase/ComputerDatabaseSimulation.java
+++ src/test/java/computerdatabase/ComputerDatabaseSimulation.java
@@ -19,33 +19,28 @@ public class ComputerDatabaseSimulation extends Simulation {
 
     FeederBuilder<String> feeder = csv("search.csv").random();
 
-    ChainBuilder search =
-        exec(http("Home").get("/"))
-            .pause(1)
-            .feed(feeder)
-            .exec(
-                http("Search")
-                    .get("/computers?f=#{searchCriterion}")
-                    .check(
-                        css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
-                    )
-            )
-            .pause(1)
-            .exec(
-                http("Select")
-                    .get("#{computerUrl}")
-                    .check(status().is(200))
-            )
-            .pause(1);
+    ChainBuilder search = exec(
+        http("Home").get("/"),
+        pause(1),
+        feed(feeder),
+        http("Search")
+            .get("/computers?f=#{searchCriterion}")
+            .check(
+                css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
+            ),
+        pause(1),
+        http("Select")
+            .get("#{computerUrl}")
+            .check(status().is(200)),
+        pause(1)
+    );
 
     // repeat is a loop resolved at RUNTIME
     ChainBuilder browse =
         // Note how we force the counter name, so we can reuse it
         repeat(4, "i").on(
-            exec(
-                http("Page #{i}")
-                    .get("/computers?p=#{i}")
-            ).pause(1)
+            http("Page #{i}").get("/computers?p=#{i}"),
+            pause(1)
         );
 
     // Note we should be using a feeder here
@@ -56,26 +51,21 @@ public class ComputerDatabaseSimulation extends Simulation {
         // let's try at max 2 times
         tryMax(2)
             .on(
-                exec(
-                    http("Form")
-                        .get("/computers/new")
-                )
-                    .pause(1)
-                    .exec(
-                        http("Post")
-                            .post("/computers")
-                            .formParam("name", "Beautiful Computer")
-                            .formParam("introduced", "2012-05-30")
-                            .formParam("discontinued", "")
-                            .formParam("company", "37")
-                            .check(
-                                status().is(
-                                    // we do a check on a condition that's been customized with
-                                    // a lambda. It will be evaluated every time a user executes
-                                    // the request
-                                    session -> 200 + ThreadLocalRandom.current().nextInt(2)
-                                )
-                            )
+                http("Form").get("/computers/new"),
+                pause(1),
+                http("Post")
+                    .post("/computers")
+                    .formParam("name", "Beautiful Computer")
+                    .formParam("introduced", "2012-05-30")
+                    .formParam("discontinued", "")
+                    .formParam("company", "37")
+                    .check(
+                        status().is(
+                            // we do a check on a condition that's been customized with
+                            // a lambda. It will be evaluated every time a user executes
+                            // the request
+                            session -> 200 + ThreadLocalRandom.current().nextInt(2)
+                        )
                     )
             )
             // if the chain didn't finally succeed, have the user exit the whole scenario
