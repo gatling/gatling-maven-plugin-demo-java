4b10ef9eec0beb2d34e7d922504093bf962bfe26
slandelle <slandelle@gatling.io>
2024-03-18 22:03:45 +0100

    chore: don't rely on gatling.conf

--- src/test/java/IDEPathHelper.java
+++ src/test/java/IDEPathHelper.java
@@ -1,7 +1,6 @@
-import java.net.URISyntaxException;
+import java.io.File;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import static java.util.Objects.requireNonNull;
+import java.util.Arrays;
 
 public class IDEPathHelper {
 
@@ -12,18 +11,18 @@ public class IDEPathHelper {
   static final Path recorderConfigFile;
 
   static {
-    try {
-      Path projectRootDir = Paths.get(requireNonNull(IDEPathHelper.class.getResource("gatling.conf"), "Couldn't locate gatling.conf").toURI()).getParent().getParent().getParent();
-      Path mavenTargetDirectory = projectRootDir.resolve("target");
+      mavenBinariesDirectory = Arrays.stream(System.getProperty("java.class.path").split(File.pathSeparator))
+              .filter(cpe -> cpe.endsWith("test-classes"))
+              .map(cpe -> Path.of(cpe))
+              .findFirst()
+              .get();
+      Path mavenTargetDirectory = mavenBinariesDirectory.getParent();
+      Path projectRootDir = mavenTargetDirectory.getParent();
       Path mavenSrcTestDirectory = projectRootDir.resolve("src").resolve("test");
 
       mavenSourcesDirectory = mavenSrcTestDirectory.resolve("java");
       mavenResourcesDirectory = mavenSrcTestDirectory.resolve("resources");
-      mavenBinariesDirectory = mavenTargetDirectory.resolve("test-classes");
       resultsDirectory = mavenTargetDirectory.resolve("gatling");
       recorderConfigFile = mavenResourcesDirectory.resolve("recorder.conf");
-    } catch (URISyntaxException e) {
-      throw new ExceptionInInitializerError(e);
-    }
   }
 }
